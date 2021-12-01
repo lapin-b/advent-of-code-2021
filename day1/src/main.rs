562dbd77caff32c615c252380fafad907a2f0e2b
l4p1n <l4p1n@l4p1n.ch>
2021-12-01 09:13:33 +0100

    Add part 1 from day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,29 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+}
+
+fn part1(){
+    let file = BufReader::new(File::open("day1/src/files/input1.txt").unwrap());
+
+    let nb_of_increased_reading = file
+        .lines()
+        .map(Result::unwrap)
+        .map(|line| i32::from_str(&line).unwrap())
+        .fold((None, 0), |prev, depth|{
+            let (previous_depth, mut nb_increased) = prev;
+
+            if let Some(prev_depth) = previous_depth {
+                if depth > prev_depth {
+                    nb_increased += 1;
+                }
+            }
+
+            (Some(depth), nb_increased)
+        }).1;
+
+    println!("Nb of increasing depth: {:?}", nb_of_increased_reading);
 }
