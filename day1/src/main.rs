7005df9b5a4443c0c334ba9eb0c3da1813124dd8
l4p1n <l4p1n@l4p1n.ch>
2021-12-02 14:37:02 +0100

    Add day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,6 +4,7 @@ use std::str::FromStr;
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1(){
@@ -26,4 +27,41 @@ fn part1(){
         }).1;
 
     println!("Nb of increasing depth: {:?}", nb_of_increased_reading);
+}
+
+fn part2(){
+    let file = BufReader::new(File::open("day1/src/files/input1.txt").unwrap());
+    let lines = file.lines()
+        .map(Result::unwrap)
+        .map(|l| i32::from_str(&l))
+        .map(Result::unwrap)
+        .collect::<Vec<i32>>();
+
+    let mut previous_sum = None;
+    let mut increase_count = 0;
+
+    for (i, line) in lines.iter().enumerate() {
+        let line = *line;
+        let line_follow_1 = lines.get(i + 1).copied();
+        let line_follow_2 = lines.get(i + 2).copied();
+
+        if line_follow_1.is_none() || line_follow_2.is_none(){
+            break;
+        }
+
+        let line_follow_1 = line_follow_1.unwrap();
+        let line_follow_2 = line_follow_2.unwrap();
+
+        let sum = line + line_follow_1 + line_follow_2;
+
+        if let Some(prev_sum) = previous_sum {
+            if sum > prev_sum {
+                increase_count += 1;
+            }
+        }
+
+        previous_sum = Some(sum);
+    }
+
+    println!("Increase counts with window of 3: {}", increase_count);
 }
