460ae59cb8267d0eef48c3592b570056dfaa7724
l4p1n <l4p1n@l4p1n.ch>
2021-12-03 10:36:39 +0100

    Use function composition when counting ones and zeroes in the diagnostic lines columns

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -10,33 +10,26 @@ fn main() {
     let lines = file
         .lines()
         .map(Result::unwrap)
-        .collect::<Vec<String>>();
+        .map(|l: String| l
+            .split("")
+            .filter(|part| !part.is_empty())
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+        )
+        .collect::<Vec<_>>();
 
     part1(&lines);
 }
 
-fn part1(lines: &[String]){
-    let prepared_lines = lines
-        .iter()
-        .map(|l| l.split("").filter(|c| !c.is_empty()).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    let columns_count = prepared_lines[0].len();
+fn part1(lines: &[Vec<String>]){
+    let columns_count = lines[0].len();
     let mut gamma_number = 0u32;
     let mut epsilon_number = 0u32;
 
     for column in 0..columns_count {
-        let mut one_count = 0;
-        let mut zero_count = 0;
-
-        for diagnostic_line in &prepared_lines {
-            let bit = diagnostic_line[column];
-            match bit {
-                "0" => zero_count += 1,
-                "1" => one_count += 1,
-                _ => unreachable!("The numbers should only be ones or zeroes, not anything else in {:?}", diagnostic_line)
-            }
-        }
+        let (one_count, zero_count) = lines
+            .iter()
+            .fold((0, 0), count_ones_and_zeroes(column));
 
         // Gamma rate: most common bit makes its way into the number
         // Epsilon rate: least common bit makes its way into the number
@@ -50,4 +43,15 @@ fn part1(lines: &[String]){
     }
 
     println!("Gamma number: {}, Epsilon number: {}, Power consumption rate: {}", gamma_number, epsilon_number, gamma_number * epsilon_number);
+}
+
+fn count_ones_and_zeroes(column: usize) -> Box<dyn FnMut((i32, i32), &Vec<String>) -> (i32, i32)> {
+    Box::new(move |(one_count, zero_count), bits|{
+        let bit = bits[column].as_str();
+        match bit {
+            "0" => (one_count, zero_count + 1),
+            "1" => (one_count + 1, zero_count),
+            _ => unreachable!("The numbers should only be ones or zeroes, not anything else in {:?}", bits)
+        }
+    })
 }
