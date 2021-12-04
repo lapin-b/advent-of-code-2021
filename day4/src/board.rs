2fe0285bacecf5a061802676315011c5d2d10275
l4p1n <l4p1n@l4p1n.ch>
2021-12-04 13:37:56 +0100

    Add lots of comments around the formulas used to fetch lines and columns of one board

--- day4/src/board.rs
+++ day4/src/board.rs
@@ -3,6 +3,9 @@ pub const LINE_COUNT_PER_GRID: usize = 5;
 
 #[derive(Debug, Clone)]
 pub struct Board {
+    // One number that have been marked will be turned into None
+    // Lines are placed one directly after the other like so:
+    // [ <line 1 items> <line 2 items> ... ]
     numbers: Vec<Option<u32>>,
 }
 
@@ -99,19 +102,33 @@ impl Board {
     }
 
     pub fn get_line(&self, n: usize) -> Option<&[Option<u32>]> {
+        // Since each line is place one after the other in the numbers vector, grabbing one line is
+        // trivial: determine the interval start on the nth line column zero and the interval end
+        // at the end of the line (a.k.a the number of columns in the line).
+
+        // Formula: The nth line we want to fetch * number of columns per line
         let interval_start = n * COLUMN_COUNT_PER_GRID;
+
+        // Formula: The nth line we want to fetch * number of columns per line + number of columns per line
         let interval_end = n * COLUMN_COUNT_PER_GRID + COLUMN_COUNT_PER_GRID;
 
         self.numbers.get(interval_start..interval_end)
     }
 
     pub fn get_column(&self, n: usize) -> Option<Vec<Option<u32>>> {
+        let mut column = Vec::with_capacity(COLUMN_COUNT_PER_GRID);
 
-        let mut column = Vec::with_capacity(LINE_COUNT_PER_GRID);
+        // Getting one column gets a bit trickier: we want the nth column on each line
+        for current_line in 0..LINE_COUNT_PER_GRID {
+            // Formula: current line we're looking at * the number of columns per line + the nth column we're
+            // looking for in the line
 
-        for col_line in 0..LINE_COUNT_PER_GRID {
-            let idx = col_line * LINE_COUNT_PER_GRID + n;
+            // `current line we're looking at * the number of columns per line` allows us to point ourselves
+            // on a specific line and `the nth column we're looking for in the line` fetches the the nth column.
+            let idx = current_line * COLUMN_COUNT_PER_GRID + n;
 
+            // In case the column does not exist or is incomplete, independently of whenever the whole column
+            // has been marked or not, we return None. Otherwise, put the number we find there on the vector
             let number_at_that_line = match self.numbers.get(idx) {
                 Some(n) => n,
                 None => return None,
