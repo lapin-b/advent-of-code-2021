8fed3cd0002ba7b680130e156cb0836ba4184984
l4p1n <l4p1n@l4p1n.ch>
2021-12-07 12:44:26 +0100

    Add day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
         .collect::<Vec<_>>();
 
     part1(&crab_positions);
+    part2(&crab_positions);
 }
 
 fn part1(positions: &[u32]) {
@@ -27,5 +28,30 @@ fn part1(positions: &[u32]) {
         cheapest_fuel_qty = min(cheapest_fuel_qty, fuel);
     }
 
-    println!("Cheapest fuel qty for everybody to align: {}", cheapest_fuel_qty);
+    println!("Cheapest fuel qty for everybody to align part 1: {}", cheapest_fuel_qty);
+}
+
+fn part2(positions: &[u32]) {
+    let max_position = *positions.iter().max().unwrap();
+    let mut cheapest_fuel_qty = u32::MAX;
+
+    for possible_position in 0..=max_position {
+        let fuel = positions
+            .iter()
+            .map(|position| {
+                let position_difference = if *position > possible_position {
+                    position - possible_position
+                } else {
+                    possible_position - position
+                };
+
+                // Fuel consumption
+                (1..=position_difference).sum::<u32>()
+            })
+            .sum::<u32>();
+
+        cheapest_fuel_qty = min(cheapest_fuel_qty, fuel);
+    }
+
+    println!("Cheapest fuel qty for everybody to align part 2: {}", cheapest_fuel_qty);
 }
