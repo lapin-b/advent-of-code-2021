73b4522e53e8dc6531708f7aa5f8871123e5491b
l4p1n <l4p1n@l4p1n.ch>
2021-12-07 13:17:59 +0100

    Refactor day 7 to use a common function to calculate the crab submarines total fuel consumption

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -11,47 +11,37 @@ fn main() {
         .map(|pos| u32::from_str(pos).unwrap())
         .collect::<Vec<_>>();
 
-    part1(&crab_positions);
-    part2(&crab_positions);
+    let fuel_req_part1 = calculate_fuel_requirements(&crab_positions, |diff| diff);
+    println!("Cheapest fuel requirement for part 1: {}", fuel_req_part1);
+
+    let fuel_req_part2 = calculate_fuel_requirements(&crab_positions, |diff| (1..=diff).sum());
+    println!("Cheapest fuel requirement for part 2: {}", fuel_req_part2);
 }
 
-fn part1(positions: &[u32]) {
-    let max_position = *positions.iter().max().unwrap();
-    let mut cheapest_fuel_qty = u32::MAX;
+fn calculate_fuel_requirements<F>(initial_positions: &[u32], fuel_calculation_fun: F) -> u32
+    where F: Fn(u32) -> u32
+{
+    let largest_initial_position = *initial_positions.iter().max().unwrap();
+    let mut minimal_fuel_requirement = u32::MAX;
 
-    for possible_position in 0..=max_position {
-        let fuel = positions
+    for possible_position in 0..=largest_initial_position {
+        let fuel_requirement = initial_positions
             .iter()
-            .map(|position| if *position > possible_position { position - possible_position } else { possible_position - position })
-            .sum::<u32>();
+            .map(|crab_position|{
+                let crab_position = *crab_position;
 
-        cheapest_fuel_qty = min(cheapest_fuel_qty, fuel);
-    }
-
-    println!("Cheapest fuel qty for everybody to align part 1: {}", cheapest_fuel_qty);
-}
-
-fn part2(positions: &[u32]) {
-    let max_position = *positions.iter().max().unwrap();
-    let mut cheapest_fuel_qty = u32::MAX;
-
-    for possible_position in 0..=max_position {
-        let fuel = positions
-            .iter()
-            .map(|position| {
-                let position_difference = if *position > possible_position {
-                    position - possible_position
+                let difference_in_position = if crab_position > possible_position {
+                    crab_position - possible_position
                 } else {
-                    possible_position - position
+                    possible_position - crab_position
                 };
 
-                // Fuel consumption
-                (1..=position_difference).sum::<u32>()
+                fuel_calculation_fun(difference_in_position)
             })
             .sum::<u32>();
 
-        cheapest_fuel_qty = min(cheapest_fuel_qty, fuel);
+        minimal_fuel_requirement = min(minimal_fuel_requirement, fuel_requirement);
     }
 
-    println!("Cheapest fuel qty for everybody to align part 2: {}", cheapest_fuel_qty);
+    minimal_fuel_requirement
 }
